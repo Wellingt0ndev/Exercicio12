d493fe77f3207736135c619614db58fedc034507
Wellingt0ndev <wellington_006@hotmail.com>
2022-09-06 16:51:16 -0300

    Atualização

--- src/exercicio12/Exercicio12.java
+++ src/exercicio12/Exercicio12.java
@@ -18,31 +18,35 @@ public class Exercicio12 {
         while (valorVeiculo != 0) {
             System.out.print("Informe o valor do veículo(Para finalizar digite 0): ");
             valorVeiculo = leitor.nextFloat();
-            System.out.println("Qual o tipo de combustível?");
-            System.out.println("1 - Álcool");
-            System.out.println("2 - Gasolina");
-            System.out.println("3 - Diesel");
-            System.out.println("Digite o número que corresponde ao combustivel: ");
-            tipoCombustivel = leitor.nextInt();
-            switch (tipoCombustivel) {
-                case 1:
-                    porcentagemDesconto = 0.25f;
-                    break;
-                case 2:
-                    porcentagemDesconto = 0.21f;
-                    break;
-                case 3:
-                    porcentagemDesconto = 0.14f;
-                    break;
-                default:
-                    System.out.println("Número inválido! Finalizando programa...");
-                    valorVeiculo = 0f;
-                    break;
+            if (valorVeiculo == 0) {
+                break;
+            } else {
+                System.out.println("Qual o tipo de combustível?");
+                System.out.println("1 - Álcool");
+                System.out.println("2 - Gasolina");
+                System.out.println("3 - Diesel");
+                System.out.println("Digite o número que corresponde ao combustivel: ");
+                tipoCombustivel = leitor.nextInt();
+                switch (tipoCombustivel) {
+                    case 1:
+                        porcentagemDesconto = 0.25f;
+                        break;
+                    case 2:
+                        porcentagemDesconto = 0.21f;
+                        break;
+                    case 3:
+                        porcentagemDesconto = 0.14f;
+                        break;
+                    default:
+                        System.out.println("Número inválido! Finalizando programa...");
+                        valorVeiculo = 0f;
+                        break;
+                }
+                valorDesconto = valorVeiculo * porcentagemDesconto;
+                valorPagar = valorVeiculo - valorDesconto;
+                System.out.println("O total de desconto foi: " + valorDesconto);
+                System.out.println("O valor do veículo com desconto: " + valorPagar);
             }
-            valorDesconto = valorVeiculo * porcentagemDesconto;
-            valorPagar = valorVeiculo - valorDesconto;
-            System.out.println("O total de desconto foi: " + valorDesconto);
-            System.out.println("O valor do veículo com desconto: " + valorPagar);
         }
     }
 }
